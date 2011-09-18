89ab2ae363e94fb2d8058f1672d0beb6a76cad44
Rodolfo Adhenawer C. Moraes <adhenawer@gmail.com>
2011-09-18 15:35:56 -0300

    adicionado cake

---
+++ cake/config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $blog = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => '127.0.0.1',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'blog',
+	);
+}
+?>
